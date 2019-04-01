f2f6fee5735f3b9db7f2c6e458913869b83730c0
SakineMazlomyar <mazlomyar12@gmail.com>
2019-04-01 14:17:07 +0200

    finishedConfig

--- src/components/hellow.tsx
+++ src/components/hellow.tsx
@@ -1,12 +1,13 @@
-import * as React from 'react';
+import React from 'react';
+
+import { centerContent } from '../css';
+
 
 export interface HelloProps { compiler: string; framework: string; }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
 
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1 style={centerContent}>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
---
+++ src/css.tsx
@@ -0,0 +1,12 @@
+import { CSSProperties } from "react";
+
+export const centerContent: CSSProperties = {
+    width: "100%",
+    height:"100%",
+    display: "flex",
+    flexDirection:"column",
+    justifyContent: "center",
+    alignItems:"center",
+    textAlign: "center"
+
+}
